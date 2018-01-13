3e8f9997400ac8a756e3e416d65b1f1ae04ce2e2
Saqeef Iqbal <nasirsaqeef2001@gmail.com>
2018-01-13 21:15:38 +0500

    Added initial toast functionality

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { ToastController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,13 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
-
-  }
-
+  constructor(public navCtrl: NavController, private toastCtrl: ToastController) { }
+  presentToast() {
+    let toast = this.toastCtrl.create({
+      message: '42',
+      duration: 3000
+    });
+    toast.present();
+  
+}
 }
